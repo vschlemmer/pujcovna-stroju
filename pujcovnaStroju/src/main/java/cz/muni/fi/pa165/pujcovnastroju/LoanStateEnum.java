8dbbccc8733c9f96e9b42d27917501cc867a55a5
xguttner <xguttner@gmail.com>
2013-10-10 18:48:27 +0200

    LoanStateEnum added

---
+++ pujcovnaStroju/src/main/java/cz/muni/fi/pa165/pujcovnastroju/LoanStateEnum.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.pujcovnastroju;
+
+/**
+ *
+ * @author gitti
+ */
+public enum LoanStateEnum {
+    BOOKED, LOANED, RETURNED;
+}
