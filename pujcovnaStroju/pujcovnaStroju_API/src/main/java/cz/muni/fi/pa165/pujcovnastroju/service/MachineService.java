1cb139f5b512e9ef48d5f69fcbe4be73d0d740d2
mertam <merta.m@gmail.com>
2013-10-30 20:49:03 +0100

    added basic spring settings,dependencies and sample MachineService implementation

--- pujcovnaStroju/pujcovnaStroju_API/src/main/java/cz/muni/fi/pa165/pujcovnastroju/service/MachineService.java
+++ pujcovnaStroju/pujcovnaStroju_API/src/main/java/cz/muni/fi/pa165/pujcovnastroju/service/MachineService.java
@@ -4,10 +4,71 @@
  */
 package cz.muni.fi.pa165.pujcovnastroju.service;
 
+import java.util.List;
+
+import cz.muni.fi.pa165.pujcovnastroju.dto.MachineDTO;
+import cz.muni.fi.pa165.pujcovnastroju.dto.MachineTypeEnumDTO;
+
+
+
 /**
  *
- * @author Matej
+ * @author Michal Merta
  */
-public class MachineService {
-    
+public interface MachineService {
+ 
+	/**
+	 * persist MachineDTO into database
+	 * 
+	 * @param MachineDTO
+	 * @return persisted object
+	 * @throws IllegalArgumentException
+	 *             when given object, it's label or type are null
+	 */
+	public MachineDTO create(MachineDTO machineDTO);
+
+	/**
+	 * updates given object
+	 * 
+	 * @param MachineDTO
+	 * @return updated object
+	 * @throws IllegalArgumentException
+	 *             when given object, it's label or type are null
+	 */
+	public MachineDTO update(MachineDTO machineDTO);
+
+	/**
+	 * returns MachineDTO with given id
+	 * 
+	 * @param id
+	 * @return MachineDTO object or null if it's not presented in database
+	 * @throws IllegalArgumentException
+	 *             wher id is null or lower then zero
+	 */
+	public MachineDTO read(Long id);
+
+	/**
+	 * deletes given MachineDTO from database
+	 * 
+	 * @param MachineDTO
+	 * @throws IllegalArgumentException
+	 *             when MachineDTO or MachineDTO.id are null
+	 */
+	public void delete(MachineDTO machine);
+
+	/**
+	 * returns all MachineDTOs presented in database
+	 * 
+	 * @return List of all MachineDTOs
+	 */
+	public List<MachineDTO> getAllMachines();
+
+	/**
+	 * returns all MachineDTOs of given MachineTypeEnumDTO
+	 * 
+	 * @param type
+	 * @return List of all MachineDTOs with given type in database
+	 */
+	public List<MachineDTO> getMachineDTOsByType(MachineTypeEnumDTO type);
+	
 }
--- pujcovnaStroju/pujcovnaStroju_Backend/src/main/java/cz/muni/fi/pa165/pujcovnastroju/serviceimpl/MachineServiceImpl.java
+++ pujcovnaStroju/pujcovnaStroju_Backend/src/main/java/cz/muni/fi/pa165/pujcovnastroju/serviceimpl/MachineServiceImpl.java
@@ -4,10 +4,95 @@
  */
 package cz.muni.fi.pa165.pujcovnastroju.serviceimpl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessResourceFailureException;
+import org.springframework.stereotype.Service;
+
+import cz.muni.fi.pa165.pujcovnastroju.converter.MachineDTOConverter;
+import cz.muni.fi.pa165.pujcovnastroju.converter.MachineTypeDTOConverter;
+import cz.muni.fi.pa165.pujcovnastroju.dao.MachineDAO;
+import cz.muni.fi.pa165.pujcovnastroju.dto.MachineDTO;
+import cz.muni.fi.pa165.pujcovnastroju.dto.MachineTypeEnumDTO;
+import cz.muni.fi.pa165.pujcovnastroju.entity.Machine;
+import cz.muni.fi.pa165.pujcovnastroju.service.MachineService;
+
 /**
- *
- * @author Matej
+ * 
+ * @author Michal Merta 374015
  */
-public class MachineServiceImpl {
-    
+@Service("machineService")
+public class MachineServiceImpl implements MachineService {
+
+	@Autowired
+	private MachineDAO machineDao;
+
+	@Override
+	public MachineDTO create(MachineDTO machineDTO) {
+		MachineDTO dto = null;
+		Machine machine = null;
+		try {
+			machine = machineDao.create(MachineDTOConverter
+					.dtoToEntity(machineDTO));
+			dto = MachineDTOConverter.entityToDto(machine);
+		} catch (IllegalArgumentException e) {
+			throw new DataAccessResourceFailureException(
+					"Error occured during storing Machine", e);
+		}
+		return dto;
+	}
+
+	@Override
+	public MachineDTO update(MachineDTO machineDTO) {
+		MachineDTO dto = null;
+		Machine machine = null;
+		try {
+			machine = machineDao.update(MachineDTOConverter
+					.dtoToEntity(machineDTO));
+			dto = MachineDTOConverter.entityToDto(machine);
+		} catch (IllegalArgumentException e) {
+			throw new DataAccessResourceFailureException(
+					"Error occured during updating Machine", e);
+		}
+		return dto;
+	}
+
+	@Override
+	public MachineDTO read(Long id) {
+		MachineDTO dto = null;
+		Machine machine = null;
+		try {
+			machine = machineDao.read(id);
+			dto = MachineDTOConverter.entityToDto(machine);
+		} catch (IllegalArgumentException e) {
+			throw new DataAccessResourceFailureException(
+					"Error occured during retrieving Machine", e);
+		}
+		return dto;
+	}
+
+	@Override
+	public void delete(MachineDTO machineDTO) {
+		try {
+			machineDao.delete(MachineDTOConverter.dtoToEntity(machineDTO));
+		} catch (IllegalArgumentException e) {
+			throw new DataAccessResourceFailureException(
+					"Error occured during deleting Machine", e);
+		}
+	}
+
+	@Override
+	public List<MachineDTO> getAllMachines() {
+		List<Machine> machineList = machineDao.getAllMachines();
+		return MachineDTOConverter.listToDto(machineList);
+	}
+
+	@Override
+	public List<MachineDTO> getMachineDTOsByType(MachineTypeEnumDTO type) {
+		List<Machine> machineList = machineDao
+				.getMachinesByType(MachineTypeDTOConverter.dtoToEntity(type));
+		return MachineDTOConverter.listToDto(machineList);
+	}
+
 }
