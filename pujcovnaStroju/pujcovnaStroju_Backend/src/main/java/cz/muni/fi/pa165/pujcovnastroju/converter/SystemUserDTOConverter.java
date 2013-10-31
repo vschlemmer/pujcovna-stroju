19b42dda944147bcd0a40b6d849ba9be31174e27
Vojtech Schlemmer <vojtech.schlemmer@gmail.com>
2013-10-31 14:54:35 +0100

    Added DTOs and converters for SystemUser and UserType

--- pujcovnaStroju/pujcovnaStroju_Backend/src/main/java/cz/muni/fi/pa165/pujcovnastroju/converter/SystemUserDTOConverter.java
+++ pujcovnaStroju/pujcovnaStroju_Backend/src/main/java/cz/muni/fi/pa165/pujcovnastroju/converter/SystemUserDTOConverter.java
@@ -1,23 +1,47 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.muni.fi.pa165.pujcovnastroju.converter;
 
 import cz.muni.fi.pa165.pujcovnastroju.dto.SystemUserDTO;
 import cz.muni.fi.pa165.pujcovnastroju.entity.SystemUser;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
- * @author Matej
+ * @author Vojtech Schlemmer
  */
 public class SystemUserDTOConverter {
 
-    public static SystemUser dtoToEntity(SystemUserDTO systemUser) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public static SystemUser dtoToEntity(SystemUserDTO userDTO) {
+        SystemUser user = new SystemUser();
+        user.setId(userDTO.getId());
+        user.setFirstName(userDTO.getFirstName());
+        user.setLastName(userDTO.getLastName());
+        user.setType(UserTypeDTOConverter.dtoToEntity(userDTO.getType()));
+        return user;
     }
     
     public static SystemUserDTO entityToDTO(SystemUser systemUser) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        SystemUserDTO userDTO = new SystemUserDTO();
+        userDTO.setId(systemUser.getId());
+        userDTO.setFirstName(systemUser.getFirstName());
+        userDTO.setLastName(systemUser.getLastName());
+        userDTO.setType(UserTypeDTOConverter.entityToDto(systemUser.getType()));
+        return userDTO;
+    }
+    
+    public static List<SystemUserDTO> listToDTO(List<SystemUser> users){
+        List<SystemUserDTO> usersDTO = new ArrayList<>();
+        for (SystemUser user : users){
+            usersDTO.add(entityToDTO(user));
+        }
+        return usersDTO;
+    }
+    
+    public static List<SystemUser> listToEntities(List<SystemUserDTO> usersDTO){
+        List<SystemUser> users = new ArrayList<>();
+        for (SystemUserDTO userDTO : usersDTO){
+            users.add(dtoToEntity(userDTO));
+        }
+        return users;
     }
 }
