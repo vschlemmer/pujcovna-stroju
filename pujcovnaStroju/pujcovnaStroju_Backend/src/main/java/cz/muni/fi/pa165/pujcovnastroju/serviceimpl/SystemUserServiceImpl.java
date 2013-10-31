d20d370e6b52d76f2e50fa41e0cf74b40fba1c14
Vojtech Schlemmer <vojtech.schlemmer@gmail.com>
2013-10-31 17:16:50 +0100

    Fixed injecting SystemUserDAO and SystemUserService

--- pujcovnaStroju/pujcovnaStroju_Backend/src/main/java/cz/muni/fi/pa165/pujcovnastroju/serviceimpl/SystemUserServiceImpl.java
+++ pujcovnaStroju/pujcovnaStroju_Backend/src/main/java/cz/muni/fi/pa165/pujcovnastroju/serviceimpl/SystemUserServiceImpl.java
@@ -6,17 +6,21 @@ import cz.muni.fi.pa165.pujcovnastroju.dao.SystemUserDAO;
 import cz.muni.fi.pa165.pujcovnastroju.dto.SystemUserDTO;
 import cz.muni.fi.pa165.pujcovnastroju.dto.UserTypeEnumDTO;
 import cz.muni.fi.pa165.pujcovnastroju.entity.SystemUser;
+import cz.muni.fi.pa165.pujcovnastroju.service.SystemUserService;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessResourceFailureException;
 
 /**
  *
  * @author Vojtech Schlemmer
  */
-public class SystemUserServiceImpl {
+public class SystemUserServiceImpl implements SystemUserService {
     
+    @Autowired
     private SystemUserDAO userDao;
-    
+
+    @Override
     public SystemUserDTO create(SystemUserDTO userDTO){
         SystemUser user = null;
         SystemUserDTO userDTO2 = null;
@@ -30,6 +34,7 @@ public class SystemUserServiceImpl {
         return userDTO2;
     }
     
+    @Override
     public SystemUserDTO read(Long id){
         SystemUser user = null;
         SystemUserDTO userDTO = null;
@@ -43,6 +48,7 @@ public class SystemUserServiceImpl {
         return userDTO;
     }
     
+    @Override
     public SystemUserDTO update(SystemUserDTO userDTO){
         SystemUser user = null;
         SystemUserDTO userDTO2 = null;
@@ -56,6 +62,7 @@ public class SystemUserServiceImpl {
         return userDTO2;
     }
     
+    @Override
     public void delete(SystemUserDTO userDTO){
         try{
             userDao.delete(SystemUserDTOConverter.dtoToEntity(userDTO));
@@ -65,11 +72,13 @@ public class SystemUserServiceImpl {
         }
     }
     
+    @Override
     public List<SystemUserDTO> findAllSystemUsers(){
         List<SystemUser> userList = userDao.findAllSystemUsers();
         return SystemUserDTOConverter.listToDTO(userList);
     }
     
+    @Override
     public List<SystemUserDTO> getSystemUsersByParams(String firstName,
 			String lastName, UserTypeEnumDTO type){
         List<SystemUser> userList = userDao.getSystemUsersByParams(firstName, 
