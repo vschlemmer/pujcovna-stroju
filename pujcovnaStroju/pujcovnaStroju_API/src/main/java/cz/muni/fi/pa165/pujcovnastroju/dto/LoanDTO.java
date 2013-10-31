fa4a73e3858a964712b3db2d8cc1b18094bdf251
xguttner <xguttner@gmail.com>
2013-10-31 13:08:50 +0100

    finishing LoanDTO, LoanStateEnumDTO and LoanService

--- pujcovnaStroju/pujcovnaStroju_API/src/main/java/cz/muni/fi/pa165/pujcovnastroju/dto/LoanDTO.java
+++ pujcovnaStroju/pujcovnaStroju_API/src/main/java/cz/muni/fi/pa165/pujcovnastroju/dto/LoanDTO.java
@@ -4,10 +4,67 @@
  */
 package cz.muni.fi.pa165.pujcovnastroju.dto;
 
+import java.util.Date;
+import java.util.List;
+
 /**
  *
- * @author Matej
+ * @author xguttner
  */
 public class LoanDTO {
     
+	private Long id;
+	private SystemUserDTO customer;
+	private List<MachineDTO> machines;
+	private Date loanTime;
+	private Date returnTime;
+	private LoanStateEnumDTO loanState;
+
+	public SystemUserDTO getCustomer() {
+	    return customer;
+	}
+
+	public void setCustomer(SystemUserDTO customer) {
+	    this.customer = customer;
+	}
+
+	public Long getId() {
+	    return id;
+	}
+
+	public void setId(Long id) {
+	    this.id = id;
+	}
+
+	public LoanStateEnumDTO getLoanState() {
+	    return loanState;
+	}
+
+	public void setLoanState(LoanStateEnumDTO loanState) {
+	    this.loanState = loanState;
+	}
+
+	public Date getLoanTime() {
+	    return loanTime;
+	}
+
+	public void setLoanTime(Date loanTime) {
+	    this.loanTime = loanTime;
+	}
+
+	public List<MachineDTO> getMachines() {
+	    return machines;
+	}
+
+	public void setMachines(List<MachineDTO> machines) {
+	    this.machines = machines;
+	}
+
+	public Date getReturnTime() {
+	    return returnTime;
+	}
+
+	public void setReturnTime(Date returnTime) {
+	    this.returnTime = returnTime;
+	}
 }
--- pujcovnaStroju/pujcovnaStroju_API/src/main/java/cz/muni/fi/pa165/pujcovnastroju/service/LoanService.java
+++ pujcovnaStroju/pujcovnaStroju_API/src/main/java/cz/muni/fi/pa165/pujcovnastroju/service/LoanService.java
@@ -1,13 +1,84 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.muni.fi.pa165.pujcovnastroju.service;
+
+import cz.muni.fi.pa165.pujcovnastroju.dto.LoanDTO;
+import cz.muni.fi.pa165.pujcovnastroju.dto.LoanStateEnumDTO;
+import cz.muni.fi.pa165.pujcovnastroju.dto.MachineDTO;
+import cz.muni.fi.pa165.pujcovnastroju.dto.SystemUserDTO;
+import java.util.Date;
+import java.util.List;
 
 /**
  *
- * @author Matej
+ * @author xguttner
  */
-public class LoanService {
-    
+public interface LoanService {
+	/**
+	 * Persist given loanDTO into database
+	 * 
+	 * @param loanDTO
+	 *            to be persisted
+	 * @return persisted loanDTO
+	 * @throws IllegalArgumentException
+	 *             when loan is null
+	 */
+	public LoanDTO create(LoanDTO loanDTO);
+
+	/**
+	 * Updates given loanDTO
+	 * 
+	 * @param loanDTO
+	 *            to be updated
+	 * @return updated loanDTO
+	 * @throws IllegalArgumentException
+	 *             when loan or loan.id is null
+	 */
+	public LoanDTO update(LoanDTO loanDTO);
+
+	/**
+	 * Reads loanDTO with given id from database
+	 * 
+	 * @param id
+	 *            of the loanDTO to be read
+	 * @return loanDTO or null if it's not presented in db
+	 * @throws IllegalArgumentException
+	 *             when id is null
+	 */
+	public LoanDTO read(Long id);
+
+	/**
+	 * Removes given loanDTO from database
+	 * 
+	 * @param id
+	 *            of the loanDTO to be deleted
+	 * @return deleted loanDTO
+	 * @throws IllegalArgumentException
+	 *             when id is null
+	 */
+	public LoanDTO delete(Long id);
+
+	/**
+	 * Returns all loanDTOs stored in the database
+	 * 
+	 * @return list of loanDTOs
+	 */
+	public List<LoanDTO> getAllLoans();
+
+	/**
+	 * Returns loanDTOs with the given parameters stored in the database
+	 * 
+	 * @param loanedFrom
+	 *            - determines the smallest loanTime of the returned loanDTOs
+	 * @param loanedTill
+	 *            - determines the largest returnTime of the returned loanDTOs
+	 * @param loanStateEnumDTO
+	 *            - determines the current state of loanDTO
+	 * @param loanedBy
+	 *            - user whose loans are to be returned
+	 * @param includedMachine
+	 *            - machine of which loans are to be returned
+	 * @return list of the loanDTOs satisfying the parameters
+	 */
+	public List<LoanDTO> getLoansByParams(Date loanedFrom, Date loanedTill,
+			LoanStateEnumDTO loanStateEnumDTO, SystemUserDTO loanedBy,
+			MachineDTO includedMachine);
 }
