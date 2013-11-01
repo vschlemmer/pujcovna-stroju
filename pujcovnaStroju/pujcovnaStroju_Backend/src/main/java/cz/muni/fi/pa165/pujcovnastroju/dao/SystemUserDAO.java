90663e94968f10386a252be42dc8ba6ee263d623
Vojtech Schlemmer <vojtech.schlemmer@gmail.com>
2013-11-01 12:03:12 +0100

    SystemUser - added IllegalArgumentException, removed transactions from DAO layer

--- pujcovnaStroju/pujcovnaStroju_Backend/src/main/java/cz/muni/fi/pa165/pujcovnastroju/dao/SystemUserDAO.java
+++ pujcovnaStroju/pujcovnaStroju_Backend/src/main/java/cz/muni/fi/pa165/pujcovnastroju/dao/SystemUserDAO.java
@@ -10,69 +10,58 @@ import cz.muni.fi.pa165.pujcovnastroju.entity.UserTypeEnum;
  * @author Vojtech Schlemmer
  */
 public interface SystemUserDAO {
-	/**
-	 * Creates a new user
-	 * 
-	 * @param user
-	 *            to be created
-	 * @return created user
-	 * @throws IllegalArgumentException
-	 *             if the user is null
-	 */
-	public SystemUser create(SystemUser user);
+    /**
+     * Creates a new user
+     * 
+     * @param user to be created
+     * @return created user
+     * @throws IllegalArgumentException if the user is null
+     */
+    public SystemUser create(SystemUser user) throws IllegalArgumentException;
 
-	/**
-	 * Reads user with given id
-	 * 
-	 * @param id
-	 *            id of the user
-	 * @return SystemUser with the given id
-	 * @throws IllegalArgumentException
-	 *             if the id is null
-	 */
-	public SystemUser read(Long id);
+    /**
+     * Reads user with given id
+     * 
+     * @param id id of the user
+     * @return SystemUser with the given id
+     * @throws IllegalArgumentException if the id is null
+     */
+    public SystemUser read(Long id) throws IllegalArgumentException;
 
-	/**
-	 * Updates given user
-	 * 
-	 * @param user
-	 *            to be updated
-	 * @return updated user
-	 * @throws IllegalArgumentException
-	 *             if the user is null
-	 */
-	public SystemUser update(SystemUser user);
+    /**
+     * Updates given user
+     * 
+     * @param user to be updated
+     * @return updated user
+     * @throws IllegalArgumentException if the user is null
+     */
+    public SystemUser update(SystemUser user) throws IllegalArgumentException;
 
-	/**
-	 * 
-	 * @param user
-	 *            to be deleted
-	 * @return deleted user
-	 * @throws IllegalArgumentException
-	 *             if the user is null
-	 */
-	public SystemUser delete(SystemUser user);
+    /**
+     * Deletes given user
+     * 
+     * @param user to be deleted
+     * @return deleted user
+     * @throws IllegalArgumentException if the user is null
+     */
+    public SystemUser delete(SystemUser user) throws IllegalArgumentException;
 
-	/**
-	 * Returns all users
-	 * 
-	 * @return list of all users
-	 */
-	public List<SystemUser> findAllSystemUsers();
+    /**
+     * Returns all users
+     * 
+     * @return list of all users
+     */
+    public List<SystemUser> findAllSystemUsers();
 
-	/**
-	 * Retrieves system users that match given parameters. If any parameter is
-	 * null, it won't be involved in lookup
-	 * 
-	 * @param firstName
-	 *            first name of users
-	 * @param lastName
-	 *            last name of users
-	 * @param type
-	 *            type of users
-	 * @return list of users that match the given parameters
-	 */
-	public List<SystemUser> getSystemUsersByParams(String firstName,
-			String lastName, UserTypeEnum type);
-
+    /**
+     * Retrieves system users that match given parameters. If any parameter is
+     * null, it won't be involved in lookup
+     * 
+     * @param firstName first name of users
+     * @param lastName last name of users
+     * @param type type of users
+     * @return list of users that match the given parameters
+     */
+    public List<SystemUser> getSystemUsersByParams(String firstName,
+                    String lastName, UserTypeEnum type);
 }
--- pujcovnaStroju/pujcovnaStroju_Backend/src/main/java/cz/muni/fi/pa165/pujcovnastroju/dao/SystemUserDAOImpl.java
+++ pujcovnaStroju/pujcovnaStroju_Backend/src/main/java/cz/muni/fi/pa165/pujcovnastroju/dao/SystemUserDAOImpl.java
@@ -17,58 +17,64 @@ import cz.muni.fi.pa165.pujcovnastroju.entity.UserTypeEnum;
  */
 public class SystemUserDAOImpl implements SystemUserDAO {
 
-	private EntityManager em;
+    private EntityManager em;
 
-	public SystemUserDAOImpl(EntityManager em) {
-		this.em = em;
-	}
+    public SystemUserDAOImpl(EntityManager em) {
+        this.em = em;
+    }
 
-	public SystemUser create(SystemUser user) {
-		em.getTransaction().begin();
-		em.persist(user);
-		em.getTransaction().commit();
-		SystemUser user1 = em.find(SystemUser.class, user.getId());
-		return user1;
-	}
+    public SystemUser create(SystemUser user) throws IllegalArgumentException {
+        if (user == null) {
+            throw new IllegalArgumentException("Argument user must be set");
+        }
+        em.persist(user);
+        SystemUser user1 = em.find(SystemUser.class, user.getId());
+        return user1;
+    }
 
-	public SystemUser read(Long id) {
-		return em.find(SystemUser.class, id);
-	}
+    public SystemUser read(Long id) throws IllegalArgumentException {
+        if (id == null) {
+            throw new IllegalArgumentException("Argument id must be set");
+        }
+        return em.find(SystemUser.class, id);
+    }
 
-	public SystemUser update(SystemUser user) {
-		em.getTransaction().begin();
-		SystemUser user1 = em.merge(user);
-		em.persist(user1);
-		em.getTransaction().commit();
-		return em.find(SystemUser.class, user1.getId());
-	}
+    public SystemUser update(SystemUser user) throws IllegalArgumentException {
+        if (user == null){
+            throw new IllegalArgumentException("Argument user must be set");
+        }    
+        SystemUser user1 = em.merge(user);
+        em.persist(user1);
+        return em.find(SystemUser.class, user1.getId());
+    }
 
-	public SystemUser delete(SystemUser user) {
-		em.getTransaction().begin();
-		SystemUser user1 = em.merge(user);
-		em.remove(user1);
-		em.getTransaction().commit();
-		return em.find(SystemUser.class, user.getId());
-	}
+    public SystemUser delete(SystemUser user) throws IllegalArgumentException {
+        if (user == null){
+            throw new IllegalArgumentException("Argument user must be set");
+        }
+        SystemUser user1 = em.merge(user);
+        em.remove(user1);
+        return em.find(SystemUser.class, user.getId());
+    }
 
-	public List<SystemUser> findAllSystemUsers() {
-		TypedQuery<SystemUser> query1 = em.createQuery(
-				"SELECT u FROM SystemUser u", SystemUser.class);
-		return query1.getResultList();
-	}
+    public List<SystemUser> findAllSystemUsers() throws IllegalArgumentException {
+        TypedQuery<SystemUser> query1 = em.createQuery(
+                        "SELECT u FROM SystemUser u", SystemUser.class);
+        return query1.getResultList();
+    }
 
-	public List<SystemUser> getSystemUsersByParams(String firstName,
-			String lastName, UserTypeEnum type) {
-		CriteriaBuilder cb = em.getCriteriaBuilder();
-		CriteriaQuery<SystemUser> cq = cb.createQuery(SystemUser.class);
-		Root<SystemUser> userRoot = cq.from(SystemUser.class);
-		cq.select(userRoot);
-		if (firstName != null)
-			cq.where(cb.equal(userRoot.get("firstName"), firstName));
-		if (lastName != null)
-			cq.where(cb.equal(userRoot.get("lastName"), lastName));
-		if (type != null)
-			cq.where(cb.equal(userRoot.get("type"), type));
-		return em.createQuery(cq).getResultList();
-	}
+    public List<SystemUser> getSystemUsersByParams(String firstName,
+                    String lastName, UserTypeEnum type) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<SystemUser> cq = cb.createQuery(SystemUser.class);
+        Root<SystemUser> userRoot = cq.from(SystemUser.class);
+        cq.select(userRoot);
+        if (firstName != null)
+            cq.where(cb.equal(userRoot.get("firstName"), firstName));
+        if (lastName != null)
+            cq.where(cb.equal(userRoot.get("lastName"), lastName));
+        if (type != null)
+            cq.where(cb.equal(userRoot.get("type"), type));
+        return em.createQuery(cq).getResultList();
+    }
 }
