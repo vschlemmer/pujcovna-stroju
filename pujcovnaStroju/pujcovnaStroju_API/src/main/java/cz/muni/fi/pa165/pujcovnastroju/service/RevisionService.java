f2c843b86c1a4fc08abb2e80bd8251749483135c
fccfreak <fucek1@gmail.com>
2013-11-01 15:44:17 +0100

    formatting

--- pujcovnaStroju/pujcovnaStroju_API/src/main/java/cz/muni/fi/pa165/pujcovnastroju/service/RevisionService.java
+++ pujcovnaStroju/pujcovnaStroju_API/src/main/java/cz/muni/fi/pa165/pujcovnastroju/service/RevisionService.java
@@ -9,25 +9,23 @@ import java.util.List;
 
 import cz.muni.fi.pa165.pujcovnastroju.dto.RevisionDTO;
 
-
 /**
  *
  * @author Matej
  */
 public interface RevisionService {
-    
-     /**
+
+    /**
      * Creates a new Revision
-     * 
+     *
      * @param revisionDTO to be created
      * @return created RevisionDTO
      * @throws IllegalArgumentException if the Revision is null
-     */ 
-    public RevisionDTO createBizRevision (RevisionDTO revisionDTO);
-    
-    
+     */
+    public RevisionDTO createBizRevision(RevisionDTO revisionDTO);
+
     /**
-	
+     *
      * Updates given revID
      *
      * @param revisionDTO to be updated
@@ -38,31 +36,33 @@ public interface RevisionService {
 
     /**
      * Deletes an Revision
+     *
      * @param revisionDTO to be deleted
-     
+     *
      * @throws IllegalArgumentException if the revisionDTO is null
      */
-    public void deleteBizRevision (RevisionDTO revisionDTO);
-    
+    public void deleteBizRevision(RevisionDTO revisionDTO);
+
     /**
      * Returns all revisions
+     *
      * @throws IllegalArgumentException if the revID is null
-     * @return list of all Revision 
+     * @return list of all Revision
      */
     public List<RevisionDTO> findAllrevisionsBizRevision();
 
     /**
      * Reads Revision with given revID
-     * 
+     *
      * @param revID of the Revision
      * @return Revision with the given revID
      * @throws IllegalArgumentException if the revID is null
      */
-    public RevisionDTO readBizRevision (Long revID);
- 
+    public RevisionDTO readBizRevision(Long revID);
+
     /**
      * Retrieves all revisions that were made in given time interval
-     * 
+     *
      * @param dateFrom date from which the revisions should be retrieved
      * @param dateTo date to which the revisions should be retrieved
      * @return list of revisions that suit the filter
