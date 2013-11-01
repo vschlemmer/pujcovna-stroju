2015004ece2e86bf75fb277aeccd1fa44998fa00
mertam <merta.m@gmail.com>
2013-11-01 17:17:17 +0100

    rewrited MachineService test

--- pujcovnaStroju/pujcovnaStroju_Backend/src/test/java/cz/muni/fi/pa165/pujcovnastroju/test/service/MachineServiceTest.java
+++ pujcovnaStroju/pujcovnaStroju_Backend/src/test/java/cz/muni/fi/pa165/pujcovnastroju/test/service/MachineServiceTest.java
@@ -1,96 +1,215 @@
 package cz.muni.fi.pa165.pujcovnastroju.test.service;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.criterion.MatchMode;
+import org.junit.Before;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Matchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+import org.springframework.dao.DataAccessResourceFailureException;
 
 import cz.muni.fi.pa165.pujcovnastroju.converter.MachineTypeDTOConverter;
+import cz.muni.fi.pa165.pujcovnastroju.dao.MachineDAO;
 import cz.muni.fi.pa165.pujcovnastroju.dto.MachineDTO;
+import cz.muni.fi.pa165.pujcovnastroju.entity.Machine;
 import cz.muni.fi.pa165.pujcovnastroju.entity.MachineTypeEnum;
 import cz.muni.fi.pa165.pujcovnastroju.service.MachineService;
+import cz.muni.fi.pa165.pujcovnastroju.serviceimpl.MachineServiceImpl;
 
+@RunWith(MockitoJUnitRunner.class)
 public class MachineServiceTest extends AbstractTest {
 
-	@Autowired
-	private MachineService service;
+	@Mock
+	MachineDAO mockMachineDao;
+
+	@InjectMocks
+	private MachineService service = new MachineServiceImpl();
+
+	@Before
+	public void setUp() {
+
+		Mockito.when(mockMachineDao.create(Matchers.any(Machine.class)))
+				.thenAnswer(new Answer<Machine>() {
+					@Override
+					public Machine answer(InvocationOnMock inv)
+							throws Throwable {
+						Object[] args = inv.getArguments();
+						return (Machine) args[0];
+
+					}
+				});
+
+		Mockito.when(mockMachineDao.update(Matchers.any(Machine.class)))
+				.thenAnswer(new Answer<Machine>() {
+					@Override
+					public Machine answer(InvocationOnMock inv)
+							throws Throwable {
+						Object[] args = inv.getArguments();
+						return (Machine) args[0];
+
+					}
+				});
+
+		Mockito.when(mockMachineDao.delete(Matchers.any(Machine.class)))
+				.thenAnswer(new Answer<Machine>() {
+					@Override
+					public Machine answer(InvocationOnMock inv)
+							throws Throwable {
+						Object[] args = inv.getArguments();
+						return (Machine) args[0];
+
+					}
+				});
+
+		Mockito.when(mockMachineDao.read(Long.valueOf(1))).thenReturn(
+				new Machine());
+
+		List<Machine> allMachines = new ArrayList<>();
+		allMachines.add(new Machine());
+		allMachines.add(new Machine());
+		Mockito.when(mockMachineDao.getAllMachines()).thenReturn(allMachines);
+
+		List<Machine> typedMachines = new ArrayList<>();
+		typedMachines.add(new Machine());
+		Mockito.when(mockMachineDao.getMachinesByType(MachineTypeEnum.CAN_OPENER))
+				.thenReturn(typedMachines);
+		Mockito.doThrow(
+				new DataAccessResourceFailureException(
+						"Error occured during creating machine."))
+				.when(mockMachineDao).create(null);
+		Mockito.doThrow(
+				new DataAccessResourceFailureException(
+						"Error occured during deleting machine."))
+				.when(mockMachineDao).delete(null);
+		Mockito.doThrow(
+				new DataAccessResourceFailureException(
+						"Error occured during updating machine."))
+				.when(mockMachineDao).update(null);
+		Mockito.doThrow(
+				new DataAccessResourceFailureException(
+						"Error occured during retrieving machine."))
+				.when(mockMachineDao).read(null);
+
+	}
 
 	@Test
-	public void testCreateNewMachine() {
-		MachineDTO m = getSampleDTO();
-		MachineDTO dto;
-		dto = service.create(m);
+	public void testCreate() {
+		MachineDTO dto = new MachineDTO();
+		try {
+			service.create(null);
+			fail();
+		} catch (DataAccessResourceFailureException e) {
+		}
+		service.create(dto);
 		assertNotNull(dto);
-		assertNotNull(dto.getId());
-		assertEquals(dto.getType(), m.getType());
-		assertEquals(dto.getLabel(), m.getLabel());
+
+		assertEquals(true, true);
 	}
 
 	@Test
-	public void testUpdateMachine() {
-		MachineDTO dto = getSampleDTO();
-		MachineDTO dtoNew = getSampleDTO();
-		dtoNew = service.create(dto);
-		assertNotNull(dtoNew);
-		dto.setId(dtoNew.getId());
-		assertEquals(dto, dtoNew);
-		dto.setLabel("small drill");
-		dto = service.update(dto);
-		assertFalse(dto.equals(dtoNew));
-		assertTrue(dto.getDecription().equals(dtoNew.getDecription()));
+	public void testDelete() {
+		try {
+			service.delete(null);
+			fail();
+		} catch (DataAccessResourceFailureException e) {
+		}
+
+		MachineDTO dto = new MachineDTO();
+		try {
+			MachineDTO deleted;
+			deleted = service.delete(dto);
+			assertNotNull(deleted);
+		} catch (Exception e) {
+			fail("Unexpected exception was thrown " + e);
+		}
 	}
 
 	@Test
-	public void testReadMachine() {
-		MachineDTO dto = getSampleDTO();
-		dto = service.create(dto);
-		MachineDTO dtoNew = service.read(dto.getId());
-		assertTrue(dto.equals(dtoNew));
+	public void testUpdate() {
+		try {
+			service.update(null);
+			fail();
+		} catch (DataAccessResourceFailureException e) {
+		}
+		try {
+			MachineDTO dto = new MachineDTO();
+			dto.setId(Long.valueOf(1));
+			dto.setType(MachineTypeDTOConverter
+					.entityToDto(MachineTypeEnum.CAN_OPENER));
+			dto.setLabel("abc");
+			MachineDTO updated = service.update(dto);
+
+			assertNotNull(updated);
+			assertEquals(dto, updated);
+		} catch (Exception e) {
+			fail("Unexpected exception was thrown " + e);
+		}
+	}
+
+	@Test
+	public void testRead() {
+		try {
+			service.read(null);
+			fail();
+		} catch (DataAccessResourceFailureException e) {
+		}
+
+		try {
+			MachineDTO dto = service.read(Long.valueOf(1));
+			assertNotNull(dto);
+		} catch (Exception e) {
+			fail("Unexpected exception was thrown " + e);
+		}
 	}
 
 	@Test
 	public void testGetAllMachines() {
-		MachineDTO dto1 = getSampleDTO();
-		MachineDTO dto2 = getSampleDTO();
-		dto2.setDecription("medium drill");
-		service.create(dto1);
-		service.create(dto2);
-		List<MachineDTO> list = service.getAllMachines();
-		assertTrue(list.size() == 2);
-		assertTrue(list.get(0).getLabel().equals(dto1.getLabel())
-				|| list.get(0).getLabel().equals(dto2.getLabel()));
-		assertTrue(list.get(1).getLabel().equals(dto1.getLabel())
-				|| list.get(1).getLabel().equals(dto2.getLabel()));
+		MachineDTO m1 = new MachineDTO();
+		MachineDTO m2 = new MachineDTO();
+		try {
+			service.create(m1);
+			service.create(m2);
+
+			List<MachineDTO> list = service.getAllMachines();
+			assertNotNull(list);
+			assertEquals(2, list.size());
+		} catch (Exception e) {
+			fail("Unexpected exception was thrown " + e);
+		}
 	}
 
 	@Test
-	public void testGetMachineByTepe() {
-		MachineDTO dto1 = getSampleDTO();
-		MachineDTO dto2 = getSampleDTO();
-		dto2.setDecription("medium buldozer");
-		dto2.setType(MachineTypeDTOConverter
-				.entityToDto(MachineTypeEnum.BULDOZER));
-		service.create(dto1);
-		service.create(dto2);
-		List<MachineDTO> list = service
-				.getMachineDTOsByType(MachineTypeDTOConverter
-						.entityToDto(MachineTypeEnum.BULDOZER));
+	public void testGetMachinesByType() {
+		MachineDTO m1 = new MachineDTO();
+		MachineDTO m2 = new MachineDTO();
+		try {
+			service.create(m1);
+			m1.setType(MachineTypeDTOConverter
+					.entityToDto(MachineTypeEnum.BULDOZER));
+			service.create(m2);
+			m2.setType(MachineTypeDTOConverter
+					.entityToDto(MachineTypeEnum.CAN_OPENER));
 
-		assertTrue(list.size() == 1);
-		assertTrue(list.get(0).getType().getTypeLabel() == MachineTypeEnum.BULDOZER.toString());
-		assertTrue(list.get(0).getDecription().equals("medium buldozer"));
+			List<MachineDTO> list = service
+					.getMachineDTOsByType(MachineTypeDTOConverter
+							.entityToDto(MachineTypeEnum.CAN_OPENER));
+			assertNotNull(list);
+			assertEquals(1, list.size());
+		} catch (Exception e) {
+			fail("Unexpected exception was thrown " + e);
+		}
 	}
 
-	private MachineDTO getSampleDTO() {
-		MachineDTO m = new MachineDTO();
-		m.setLabel("drill");
-		m.setType(MachineTypeDTOConverter.entityToDto(MachineTypeEnum.DRILL));
-		m.setDecription("big drill");
-		return m;
-	}
 }
