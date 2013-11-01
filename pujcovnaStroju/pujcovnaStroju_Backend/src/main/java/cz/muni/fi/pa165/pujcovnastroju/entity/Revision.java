8b80fe8340d67703d2c8ed5b7f0d80185378c852
fccfreak <fucek1@gmail.com>
2013-11-01 15:46:00 +0100

    formatting

--- pujcovnaStroju/pujcovnaStroju_Backend/src/main/java/cz/muni/fi/pa165/pujcovnastroju/entity/Revision.java
+++ pujcovnaStroju/pujcovnaStroju_Backend/src/main/java/cz/muni/fi/pa165/pujcovnastroju/entity/Revision.java
@@ -12,107 +12,104 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 
 /**
- * 
+ *
  * @author Matej Fucek
  */
-
 @Entity
 public class Revision implements Serializable {
-	/**
-	 * auto generated serial id
-	 */
-	private static final long serialVersionUID = -1600141775151321009L;
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long revID;
-	private String comment;
 
-	@Temporal(javax.persistence.TemporalType.TIMESTAMP)
-	private Date revDate;
+    /**
+     * auto generated serial id
+     */
+    private static final long serialVersionUID = -1600141775151321009L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long revID;
+    private String comment;
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    private Date revDate;
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE,
+        CascadeType.REFRESH})
+    private Machine machine;
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE,
+        CascadeType.REFRESH})
+    private SystemUser systemUser;
 
-	@ManyToOne(cascade = { CascadeType.PERSIST, CascadeType.MERGE,
-			CascadeType.REFRESH })
-	private Machine machine;
-	@ManyToOne(cascade = { CascadeType.PERSIST, CascadeType.MERGE,
-			CascadeType.REFRESH })
-	private SystemUser systemUser;
+    public Long getRevID() {
+        return revID;
+    }
 
-	public Long getRevID() {
-		return revID;
-	}
+    public void setRevID(Long revID) {
+        this.revID = revID;
+    }
 
-	public void setRevID(Long revID) {
-		this.revID = revID;
-	}
+    public String getComment() {
+        return comment;
+    }
 
-	public String getComment() {
-		return comment;
-	}
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
+    public Machine getMachine() {
+        return machine;
+    }
 
-	public Machine getMachine() {
-		return machine;
-	}
+    public void setMachine(Machine machine) {
+        this.machine = machine;
+    }
 
-	public void setMachine(Machine machine) {
-		this.machine = machine;
-	}
+    public Date getRevDate() {
+        return revDate;
+    }
 
-	public Date getRevDate() {
-		return revDate;
-	}
+    public void setRevDate(Date revDate) {
+        this.revDate = revDate;
+    }
 
-	public void setRevDate(Date revDate) {
-		this.revDate = revDate;
-	}
+    public SystemUser getSystemUser() {
+        return systemUser;
+    }
 
-	public SystemUser getSystemUser() {
-		return systemUser;
-	}
+    public void setSystemUser(SystemUser systemUser) {
+        this.systemUser = systemUser;
+    }
 
-	public void setSystemUser(SystemUser systemUser) {
-		this.systemUser = systemUser;
-	}
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + (this.revID != null ? this.revID.hashCode() : 0);
+        hash = 97 * hash + (this.comment != null ? this.comment.hashCode() : 0);
+        hash = 97 * hash + (this.revDate != null ? this.revDate.hashCode() : 0);
+        hash = 97 * hash + (this.machine != null ? this.machine.hashCode() : 0);
+        return hash;
+    }
 
-	@Override
-	public int hashCode() {
-		int hash = 7;
-		hash = 97 * hash + (this.revID != null ? this.revID.hashCode() : 0);
-		hash = 97 * hash + (this.comment != null ? this.comment.hashCode() : 0);
-		hash = 97 * hash + (this.revDate != null ? this.revDate.hashCode() : 0);
-		hash = 97 * hash + (this.machine != null ? this.machine.hashCode() : 0);
-		return hash;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		final Revision other = (Revision) obj;
-		if (this.revID != other.revID
-				&& (this.revID == null || !this.revID.equals(other.revID))) {
-			return false;
-		}
-		if ((this.comment == null) ? (other.comment != null) : !this.comment
-				.equals(other.comment)) {
-			return false;
-		}
-		if ((this.revDate == null) ? (other.revDate != null) : !this.revDate
-				.equals(other.revDate)) {
-			return false;
-		}
-		if ((this.machine == null) ? (other.machine != null) : !this.machine
-				.equals(other.machine)) {
-			return false;
-		}
-		return true;
-	}
-
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Revision other = (Revision) obj;
+        if (this.revID != other.revID
+                && (this.revID == null || !this.revID.equals(other.revID))) {
+            return false;
+        }
+        if ((this.comment == null) ? (other.comment != null) : !this.comment
+                .equals(other.comment)) {
+            return false;
+        }
+        if ((this.revDate == null) ? (other.revDate != null) : !this.revDate
+                .equals(other.revDate)) {
+            return false;
+        }
+        if ((this.machine == null) ? (other.machine != null) : !this.machine
+                .equals(other.machine)) {
+            return false;
+        }
+        return true;
+    }
 }
