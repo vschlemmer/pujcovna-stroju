18a0c5a12e4901aa909262a7e61e1d074c5733e4
Vojtech Schlemmer <vojtech.schlemmer@gmail.com>
2013-11-01 13:42:10 +0100

    All converters now return null when null parameter passed

--- pujcovnaStroju/pujcovnaStroju_Backend/src/main/java/cz/muni/fi/pa165/pujcovnastroju/converter/UserTypeDTOConverter.java
+++ pujcovnaStroju/pujcovnaStroju_Backend/src/main/java/cz/muni/fi/pa165/pujcovnastroju/converter/UserTypeDTOConverter.java
@@ -10,14 +10,16 @@ import cz.muni.fi.pa165.pujcovnastroju.entity.UserTypeEnum;
  */
 public class UserTypeDTOConverter {
     public static UserTypeEnum dtoToEntity(UserTypeEnumDTO dto) {
-            UserTypeEnum type = UserTypeEnum.valueOf(dto.getTypeLabel());
-            return type;
+        if (dto == null) return null;
+        UserTypeEnum type = UserTypeEnum.valueOf(dto.getTypeLabel());
+        return type;
     }
 
     public static UserTypeEnumDTO entityToDto(UserTypeEnum type) {
-            UserTypeEnumDTO dto = new UserTypeEnumDTO();
-            dto.setId(Long.valueOf(type.ordinal()));
-            dto.setTypeLabel(type.name());
-            return dto;
+        if (type == null) return null;
+        UserTypeEnumDTO dto = new UserTypeEnumDTO();
+        dto.setId(Long.valueOf(type.ordinal()));
+        dto.setTypeLabel(type.name());
+        return dto;
     }
 }
