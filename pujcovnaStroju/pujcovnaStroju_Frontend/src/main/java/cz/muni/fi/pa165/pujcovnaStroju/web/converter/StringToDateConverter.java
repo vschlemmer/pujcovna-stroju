4e3350696c60bf78fc50863c23fa9feb447109f9
xguttner <xguttner@gmail.com>
2013-11-21 02:27:38 +0100

    StringToDateConverter: enables correctly process dates from datepicker inputs

---
+++ pujcovnaStroju/pujcovnaStroju_Frontend/src/main/java/cz/muni/fi/pa165/pujcovnaStroju/web/converter/StringToDateConverter.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.pujcovnaStroju.web.converter;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.springframework.core.convert.converter.Converter;
+
+/**
+ *
+ * @author gitti
+ */
+public class StringToDateConverter implements
+		Converter<String, Date> {
+	public Date convert(String source) {
+	    try {
+		System.out.println("convert date");
+		Date date = new SimpleDateFormat("yyyy-MM-dd HH:mm").parse(source);
+		return date;
+	    } catch (ParseException ex) {
+		return null;
+	    }
+	}
+}
