7958fc2532c3bf8126eb833cc0b55ff4bd4f61a6
mertam <merta.m@gmail.com>
2014-01-15 19:36:55 +0100

    fixed automatic admin account creation

--- pujcovnaStroju/pujcovnaStroju_Backend/src/main/java/cz/muni/fi/pa165/pujcovnastroju/security/UserDetailsServiceImpl.java
+++ pujcovnaStroju/pujcovnaStroju_Backend/src/main/java/cz/muni/fi/pa165/pujcovnastroju/security/UserDetailsServiceImpl.java
@@ -1,39 +1,58 @@
 package cz.muni.fi.pa165.pujcovnastroju.security;
 
-import cz.muni.fi.pa165.pujcovnastroju.dao.SystemUserDAO;
-import cz.muni.fi.pa165.pujcovnastroju.entity.SystemUser;
 import java.util.HashSet;
 import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import cz.muni.fi.pa165.pujcovnastroju.dao.SystemUserDAO;
+import cz.muni.fi.pa165.pujcovnastroju.entity.SystemUser;
+import cz.muni.fi.pa165.pujcovnastroju.entity.UserTypeEnum;
 
 /**
- *
+ * 
  * @author Vojtech Schlemmer
  */
 @Service("userDetailsService")
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-    @Autowired
-    private SystemUserDAO userDao;
-    
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        SystemUser user01 = null;
-        try{
-            user01 = userDao.getSystemUserByUsername(username);
-        } catch (Exception e){
-            System.err.println(e.getMessage());
-        }
-        Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
-        authorities.add(new SimpleGrantedAuthority(user01.getType().name()));
-        return new UserDetailsImpl(user01, true,
-                true, true, true, authorities);
-    }
-    
+	@Autowired
+	private SystemUserDAO userDao;
+
+	@Transactional
+	@Override
+	public UserDetails loadUserByUsername(String username)
+			throws UsernameNotFoundException {
+		SystemUser user01 = null;
+
+		try {
+			user01 = userDao.getSystemUserByUsername(username);
+			if (user01 == null && username.equals("admin")) {
+				user01 = new SystemUser();
+				user01.setFirstName("admin");
+				user01.setLastName("admin");
+				user01.setUsername("admin");
+				PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+				String encodedPassword = passwordEncoder.encode("admin");
+				user01.setPassword(encodedPassword);
+				user01.setType(UserTypeEnum.ADMINISTRATOR);
+				user01 = userDao.create(user01);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
+		authorities.add(new SimpleGrantedAuthority(user01.getType().name()));
+		return new UserDetailsImpl(user01, true, true, true, true, authorities);
+	}
+
 }
--- pujcovnaStroju/pujcovnaStroju_Frontend/src/main/java/cz/muni/fi/pa165/pujcovnaStroju/web/controller/DefaultController.java
+++ pujcovnaStroju/pujcovnaStroju_Frontend/src/main/java/cz/muni/fi/pa165/pujcovnaStroju/web/controller/DefaultController.java
@@ -1,97 +1,77 @@
 package cz.muni.fi.pa165.pujcovnaStroju.web.controller;
 
-import cz.muni.fi.pa165.pujcovnastroju.converter.UserTypeDTOConverter;
-import cz.muni.fi.pa165.pujcovnastroju.dto.SystemUserDTO;
-import cz.muni.fi.pa165.pujcovnastroju.entity.SystemUser;
-import cz.muni.fi.pa165.pujcovnastroju.entity.UserTypeEnum;
-import cz.muni.fi.pa165.pujcovnastroju.security.UserDetailsServiceImpl;
-import cz.muni.fi.pa165.pujcovnastroju.service.SystemUserService;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import cz.muni.fi.pa165.pujcovnastroju.dto.SystemUserDTO;
+import cz.muni.fi.pa165.pujcovnastroju.service.SystemUserService;
+
 /**
- * Controller handling root request 
+ * Controller handling root request
+ * 
  * @author Michal Merta
- *
+ * 
  */
 @Controller
 public class DefaultController {
 
-    private static SystemUserService userService;
-    private boolean defaultAdminCreated = false;
+	private static SystemUserService userService;
 
-    @Autowired
-    public DefaultController(SystemUserService userService) {
-        this.userService = userService;
-    }
-    
-    @RequestMapping("/")
-    public String viewIndes(ModelMap model) {
-        model.addAttribute("pageTitle", "lang.listMachinesTitle");
-        addHeaderFooterInfo(model);
-        if (!defaultAdminCreated){
-            createDefaultAdmin();
-            defaultAdminCreated = true;
-        }
-        return "index";
-    }
-	
+	@Autowired
+	public DefaultController(SystemUserService userService) {
+		this.userService = userService;
+	}
+
+	@RequestMapping("/")
+	public String viewIndes(ModelMap model) {
+		model.addAttribute("pageTitle", "lang.listMachinesTitle");
+		addHeaderFooterInfo(model);
+		return "index";
+	}
+
 	/**
 	 * adds header and footer content to rendered page
 	 */
 	static ModelMap addHeaderFooterInfo(ModelMap model) {
 		model.addAttribute("pageFooter", "lang.footer");
 		model.addAttribute("pageHeader", "lang.header");
-                addLeftMenuSecurity(model);
+		addLeftMenuSecurity(model);
 		return model;
 	}
-	
+
 	/**
 	 * adds static left menu content to rendered page
+	 * 
 	 * @param model
 	 * @return
 	 */
 	static ModelMap addLeftMenu(ModelMap model) {
 		return model;
 	}
-        
-        static ModelMap addLeftMenuSecurity(ModelMap model){
-            model.addAttribute("userType", getLoggedUserType());
-            return model;
-        }
-        
-        static void createDefaultAdmin(){
-            SystemUserDTO user = new SystemUserDTO();
-            user.setFirstName("admin");
-            user.setLastName("admin");
-            user.setUsername("admin");
-            PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-            String hashedPassword = passwordEncoder.encode("admin");
-            user.setPassword(hashedPassword);
-            user.setType(UserTypeDTOConverter.entityToDto(UserTypeEnum.ADMINISTRATOR));
-            userService.create(user);
-        }
-        
-        static String getLoggedUserType(){
-            String userType = "";
-            if (SecurityContextHolder.getContext().getAuthentication() != null &&
-                SecurityContextHolder.getContext().getAuthentication().isAuthenticated()){
-                if (SecurityContextHolder.getContext().getAuthentication() != null){
-                    String authUserString = SecurityContextHolder.getContext().
-                            getAuthentication().getName();
-                    SystemUserDTO authUser = userService.getSystemUserByUsername(authUserString);
-                    if (authUser != null){
-                        userType = authUser.getType().getTypeLabel();
-                    }
-                }
-            }
-            return userType;
-        }
+
+	static ModelMap addLeftMenuSecurity(ModelMap model) {
+		model.addAttribute("userType", getLoggedUserType());
+		return model;
+	}
+
+	static String getLoggedUserType() {
+		String userType = "";
+		if (SecurityContextHolder.getContext().getAuthentication() != null
+				&& SecurityContextHolder.getContext().getAuthentication()
+						.isAuthenticated()) {
+			if (SecurityContextHolder.getContext().getAuthentication() != null) {
+				String authUserString = SecurityContextHolder.getContext()
+						.getAuthentication().getName();
+				SystemUserDTO authUser = userService
+						.getSystemUserByUsername(authUserString);
+				if (authUser != null) {
+					userType = authUser.getType().getTypeLabel();
+				}
+			}
+		}
+		return userType;
+	}
 }
